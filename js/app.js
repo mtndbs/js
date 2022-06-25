59c0ba56c7d0c8e66f568170eed45577e325fab1
mtndbs <mtndbs@gmail.com>
2022-06-25 22:53:02 +0300

    string promt - fine

--- js/app.js
+++ js/app.js
@@ -1,44 +1,34 @@
-let allAge = "0-99"
-let acces = "Entery permit"
-let accesDenied = "Aproval denied"
+let allAge = "0-99";
+let acces = "Entery permit";
+let accesDenied = "Aproval denied";
 
+let age = prompt("Hellow there" + " " + "whats is your age?" + " " + allAge);
 
-let age = prompt("Hellow there" + " " + "whats is your age?" + " " + allAge )
-
-if(age  >= 18){
-    console.log(acces)
-
-    let meat = prompt("Do you like to eat MEAT?" + " " + "yes / no")
-
-    if(meat = "yes"){
-let hungry = prompt("Do you hungry?" + " " + "yes / no")
-            if(hungry = "yes"){
-                document.write("Shnizel and Rise")
-            }else{
-            document.write("Shnizel Only")}
-        }else{
-    document.write("Salad and Chips")
+if (age >= 18) {
+  console.log(acces);
+  let meat = prompt("Do you like to eat MEAT?" + " " + "yes / no"); 
+  if ((meat == "yes")) {
+    let hungry = prompt("Do you hungry?" + " " + "yes / no");
+    if ((hungry == "yes")) {
+        document.write("Shnizel and Rise");
+        document.getElementById('myImage').src='https://www.pngall.com/wp-content/uploads/13/Schnitzel-PNG-Pic.png'
+        document.getElementById('myImage2').src='https://www.mastermatkonim.co.il/wp-content/uploads/2020/02/20200218_113610.jpg'
+      }else if(hungry !== "no" && hungry !=="yes"){
+        document.write(" invalid answer")
+    } else {
+      document.write("Shnizel Only");
+      document.getElementById('myImage2').src='https://www.pngall.com/wp-content/uploads/13/Schnitzel-PNG-Pic.png'
     }
-}else{
-console.log(accesDenied);
-document.write("ACCES DENIED!")
-        
+  } else {
+      document.write("Salad and Chips");
+      document.getElementById('myImage').src='https://i0.wp.com/smittenkitchen.com/wp-content/uploads//2007/07/700544972_564e118f79_b.jpg?fit=1024%2C680&ssl=1'
+      document.getElementById('myImage2').src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1xGLRKCk3mVpZt06iKTXPr__FIx12yeteRg&usqp=CAU'
+  }
+} else {
+  console.log(accesDenied);
+  document.write("ACCES DENIED!");
 }
 
-
-    
-
-
-
-
-
-
-// document.getElementById('myImage2').src='https://www.pngall.com/wp-content/uploads/13/Schnitzel-PNG-Pic.png'
-
-// document.getElementById('myImage').src='https://i0.wp.com/smittenkitchen.com/wp-content/uploads//2007/07/700544972_564e118f79_b.jpg?fit=1024%2C680&ssl=1'
-// document.getElementById('myImage2').src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1xGLRKCk3mVpZt06iKTXPr__FIx12yeteRg&usqp=CAU'
-// document.getElementById('myImage').src='https://www.mastermatkonim.co.il/wp-content/uploads/2020/02/20200218_113610.jpg'
-// document.getElementById('myImage').src='https://www.pngall.com/wp-content/uploads/13/Schnitzel-PNG-Pic.png'
 
 
 // // let password = prompt("you neeed atleest 6 cheracters")
@@ -48,7 +38,5 @@ document.write("ACCES DENIED!")
 // password = prompt("less chars")
 // if(password.length == 6)
 
-
 // }
 // }
-
